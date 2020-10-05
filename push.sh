f73c18f493575379fcba4ea8e1dde26eaa9ab2f6
Yosuke OTSUKI <y.otsuki30@gmail.com>
2020-10-06 00:22:40 +0900

    fixed url

--- push.sh
+++ push.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-git push https://github.com/yosukesan.github.io.git gh-pages:master
+# extract output page
+ghp-import output
+
+# push to github io page
+git push https://github.com/yosukesan/yosukesan.github.io.git gh-pages:master
